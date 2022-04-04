9ba8033f32da9b9c49c91c2660af342ae404c2a9
Jackson Hellmers <shaqsonjackson@gmail.com>
2022-04-04 17:31:28 -0500

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -29,40 +29,31 @@ class stitch:
             print("Not enough optimal points found")
         return h_3x3
 
-    def get_stitched_image(self, img1, img2, M):
+    def createStitch(self, img1, img2, M):
 
-        # Get width and height of input images
         w1, h1 = img1.shape[:2]
         w2, h2 = img2.shape[:2]
 
-        # Get the canvas dimesions
         img1_dims = np.float32([[0, 0], [0, w1], [h1, w1], [h1, 0]]).reshape(-1, 1, 2)
         img2_dims_temp = np.float32([[0, 0], [0, w2], [h2, w2], [h2, 0]]).reshape(-1, 1, 2)
 
-        # Get relative perspective of second image
         img2_dims = cv2.perspectiveTransform(img2_dims_temp, M)
 
-        # Resulting dimensions
         result_dims = np.concatenate((img1_dims, img2_dims), axis=0)
 
-        # Getting images together
-        # Calculate dimensions of match points
         [x_min, y_min] = np.int32(result_dims.min(axis=0).ravel())
         [x_max, y_max] = np.int32(result_dims.max(axis=0).ravel())
 
-        # Create output array after affine transformation
         transform_dist = [-x_min, -y_min]
         transform_array = np.array([[1, 0, transform_dist[0]],
                                     [0, 1, transform_dist[1]],
                                     [0, 0, 1]])
 
-        # Warp images to get the resulting image
         result_img = cv2.warpPerspective(img2, transform_array.dot(M),
                 (x_max - x_min, y_max - y_min), flags=cv2.INTER_LINEAR)
         result_img[transform_dist[1]:w1 + transform_dist[1],
         transform_dist[0]:h1 + transform_dist[0]] = img1
 
-        # Return the result
         return result_img
 
     def getSIFTFeatures(self, img):
@@ -78,7 +69,7 @@ if __name__ == "__main__":
     img1_gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
     img2_gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
     h12 = stitcher.match(im2, im1)
-    result = stitcher.get_stitched_image(im2, im1, h12)
+    result = stitcher.createStitch(im2, im1, h12)
     cv2.imshow("result", result)
     cv2.waitKey(0)
     cv2.imwrite("mountain_result.png",result)
